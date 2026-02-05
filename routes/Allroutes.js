9c2222ffb15846328a405b452cbee02d9ca863b1
mervatElRayes <mervatel99@gmail.com>
2026-02-05 02:53:54 +0200

    lesson4

--- routes/Allroutes.js
+++ routes/Allroutes.js
@@ -1,42 +1,40 @@
-const express = require('express')
-const router = express.Router()
-const userController = require("../controllers/userController")
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const AuthUser = require("../models/authUser");
+const { triggerAsyncId } = require("node:async_hooks");
+
 // level 2
 router.get("/", (req, res) => {
-res.render("welcome")
+  res.render("welcome");
 });
 
 router.get("/login", (req, res) => {
-res.render("auth/login")
-
-
+  res.render("auth/login");
 });
 
 router.get("/signup", (req, res) => {
-res.render("auth/signup")
-
-
+  res.render("auth/signup");
 });
 
+router.post("/signup", async (req, res) => {
+const result = await AuthUser.create(req.body)
+try {
+     res.redirect("/")
+} catch (error) {
+   console.log(error); 
+}
+    console.log(result);
 
-
-
-
-
-
-
+});
 
 //level 1
 // get Request
 router.get("/home", userController.user_index_get);
 
-
-
 router.get("/edit/:id", userController.user_edit_get);
 
 router.get("/view/:id", userController.user_view_get);
-
-
 
 // search Request
 router.post("/search", userController.user_search_post);
@@ -47,17 +45,4 @@ router.delete("/edit/:id", userController.user_delete);
 //Put Or Edit Request
 router.put("/edit/:id", userController.user_put);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
